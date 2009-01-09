6d6dea609de33658540f40e07ebfdf49ae4d39d7
rlskoeser <rebecca.s.koeser@emory.edu>
2009-01-09 00:31:40 +0000

    added link to python-fedoracommons prereqs install page

--- src/test.py
+++ src/test.py
@@ -3,7 +3,8 @@
 ## to use fedora objects in python console, run:
 ## python -i test.py
 
-from pythonFedoraCommons import fedoraClient
+from pythonFedoraCommons import fedoraClient,risearch
 
 client = fedoraClient.ClientFactory()
-fedora = client.getClient("http://wilson:6080/fedora", "fedoraAdmin", "fedoraAdmin", "2.2")
+fedora = client.getClient("http://wilson:7080/fedora", "fedoraAdmin", "fedoraAdmin", "2.2")
+ri = risearch.Risearch("http://wilson:6080/fedora")
